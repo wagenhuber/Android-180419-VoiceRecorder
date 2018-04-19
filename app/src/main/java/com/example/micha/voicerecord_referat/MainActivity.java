49133f214b3fd3f9de16ea48c5cf09d02d044ae2
wagenhuber <wagenhuber@gmx.net>
2018-04-19 08:51:42 +0200

    fertig

--- app/src/main/java/com/example/micha/voicerecord_referat/MainActivity.java
+++ app/src/main/java/com/example/micha/voicerecord_referat/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+
 import java.io.IOException;
 
 import static android.Manifest.permission.RECORD_AUDIO;
@@ -21,9 +22,9 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btnRecord, btnStop, btnPlay, btnStopPlayRecording;
     String AudioSavePathInDevice = null;
-    MediaRecorder mediaRecorder ;
+    MediaRecorder mediaRecorder;
     public static final int RequestPermissionCode = 1;
-    MediaPlayer mediaPlayer ;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +43,26 @@ public class MainActivity extends AppCompatActivity {
         btnRecord.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (checkPermission()) {
+                    AudioSavePathInDevice = Environment.getExternalStorageDirectory().getAbsolutePath() +
+                            "/" + "AudioRecording.3gp";
 
+                    mediaRecorderReady();
+
+                    try {
+                        mediaRecorder.prepare();
+                        mediaRecorder.start();
+                        btnRecord.setEnabled(false);
+                        btnStop.setEnabled(true);
+                        Toast.makeText(MainActivity.this, "Recording started", Toast.LENGTH_SHORT).show();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                } else {
+                    requestPermission();
+                }
 
             }
         });
@@ -51,8 +71,11 @@ public class MainActivity extends AppCompatActivity {
         btnStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
+                mediaRecorder.stop();
+                btnStop.setEnabled(false);
+                btnPlay.setEnabled(true);
+                btnRecord.setEnabled(true);
+                btnStopPlayRecording.setEnabled(true);
 
             }
         });
@@ -60,23 +83,51 @@ public class MainActivity extends AppCompatActivity {
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) throws IllegalArgumentException, SecurityException, IllegalStateException {
+                btnStop.setEnabled(false);
+                btnRecord.setEnabled(false);
+                btnStopPlayRecording.setEnabled(true);
 
-
-
+                mediaPlayer = new MediaPlayer();
+                try {
+                    mediaPlayer.setDataSource(AudioSavePathInDevice);
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                    Toast.makeText(MainActivity.this, "Recording Playing", Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
         btnStopPlayRecording.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                btnStop.setEnabled(false);
+                btnRecord.setEnabled(true);
+                btnStopPlayRecording.setEnabled(false);
+                btnPlay.setEnabled(true);
 
+                if (mediaPlayer != null) {
+                    mediaPlayer.stop();
+                    mediaPlayer.release();
+                    mediaRecorderReady();
 
+                }
 
             }
         });
 
 
     }
+
+    private void mediaRecorderReady() {
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        mediaRecorder.setOutputFile(AudioSavePathInDevice);
+    }
+
 
     private void requestPermission() {
 
@@ -96,9 +147,8 @@ public class MainActivity extends AppCompatActivity {
                     if (StoragePermission && RecordPermission) {
 
                         Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_LONG).show();
-                    }
-                    else {
-                        Toast.makeText(MainActivity.this,"Permission Denied",Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Permission Denied", Toast.LENGTH_LONG).show();
 
                     }
                 }
